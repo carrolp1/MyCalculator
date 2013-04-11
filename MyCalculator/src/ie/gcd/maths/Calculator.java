d8ce1d603cf8bc6290fba5acbdca1a87479cbe60
2843951 <2843951@JS101MAC-031.student.gcd.ie>
2013-04-11 13:02:10 +0100

    Initial Commit

---
+++ MyCalculator/src/ie/gcd/maths/Calculator.java
@@ -0,0 +1,37 @@
+package ie.gcd.maths;
+
+public class Calculator {
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
