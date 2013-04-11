aba89e1c8020bbedfe6529cd0959d24fad4019cd
2843951 <2843951@JS101MAC-031.student.gcd.ie>
2013-04-11 13:20:50 +0100

    Branch Setup

---
+++ MyCalculator/src/ie/gcd/maths/ScientificCalc.java
@@ -0,0 +1,19 @@
+package ie.gcd.maths;
+
+public class ScientificCalc {
+	
+	public int add (int x, int y){
+		
+		return x-y;
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
